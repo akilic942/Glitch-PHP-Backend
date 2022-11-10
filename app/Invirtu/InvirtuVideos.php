4e1767c16ced8f5554035538effa04e505145380
Devin Dixon <devindixon@Devins-MBP.lan>
2022-11-09 20:33:05 -0500

    Implementing video routes

--- app/Invirtu/InvirtuVideos.php
+++ app/Invirtu/InvirtuVideos.php
@@ -10,15 +10,67 @@ class InvirtuVideos extends InvirtuResource
 {
 
     /**
-     * Gets a content
+     * Get a list of video content.
      *
-     * @see    https://developers.thinkific.com/api/api-documentation/
+     * @see    https://developers.bingewave.com/docs/videos#list
      * @param  string $id
      * @return stdClass
      * @throws Exception
      */
-    public function get($id)
+    public function list(array $query = [])
     {
-        return $this->client->get('contents/'.$id);
+        return $this->client->get('/videos', $query);
+    }
+
+    /**
+     * Create a video/pre-recordend content
+     *
+     * @see    https://developers.bingewave.com/docs/videos#create
+     * @param  string $id
+     * @return stdClass
+     * @throws Exception
+     */
+    public function create(array $data, array $query = [])
+    {
+        return $this->client->post('/videos', $data, $query);
+    }
+
+    /**
+     * Update a video file.
+     *
+     * @see    https://developers.bingewave.com/docs/videos#update
+     * @param  string $id
+     * @return stdClass
+     * @throws Exception
+     */
+    public function update(string $video_id, array $data, array $query = [])
+    {
+        return $this->client->put('/videos/' . $video_id, $data, $query);
+    }
+
+    /**
+     * Retrieve a single video file.
+     *
+     * @see    https://developers.bingewave.com/docs/videos#view
+     * @param  string $id
+     * @return stdClass
+     * @throws Exception
+     */
+    public function get(string $video_id, array $query = [])
+    {
+        return $this->client->get('/videos/' . $video_id, $query);
+    }
+
+    /**
+     * Deletes a video file
+     *
+     * @see    https://developers.bingewave.com/docs/videos#delete
+     * @param  string $id
+     * @return stdClass
+     * @throws Exception
+     */
+    public function delete(string $video_id, array $data, array $query = [])
+    {
+        return $this->client->delete('/videos/' . $video_id, $data, $query);
     }
 }
