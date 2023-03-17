283dcdd5fcd76cb496d192b7678b3e3524c0dae6
Devin Dixon <devindixon@Devins-MacBook-Pro.local>
2023-03-17 01:08:52 -0400

    Adding recording option

--- app/Http/Controllers/RecordingController.php
+++ app/Http/Controllers/RecordingController.php
@@ -11,58 +11,7 @@ use Illuminate\Http\Request;
 
 class RecordingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Recording  $recording
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Recording $recording)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Recording  $recording
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Recording $recording)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -71,19 +20,67 @@ class RecordingController extends Controller
      * @param  \App\Models\Recording  $recording
      * @return \Illuminate\Http\Response
      */
+
+    /**
+     * @OA\Put(
+     *     path="events/{uuid}/recording/{subid}",
+     *     summary="Update a recording related to the event.",
+     *     description="Update a recording related to the event.",
+     *     operationId="updateEventRecording",
+     *     tags={"Event Route"},
+     *     security={{"bearer": {}}},
+     *      @OA\Parameter(
+     *         name="uuid",
+     *         in="path",
+     *         description="UUID of the event",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *      @OA\Parameter(
+     *         name="subid",
+     *         in="path",
+     *         description="UUID of the recording",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent( ref="#/components/schemas/Recordings")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *          @OA\JsonContent( ref="#/components/schemas/EventFull")
+     *     ),
+     *     @OA\Response(
+     *      response=403,
+     *      description="Access denied to event.",
+     *      @OA\JsonContent(
+     *          @OA\Property(property="message", type="Access denined to the event resource.")
+     *       )
+     *     ) 
+     * )
+     *     
+     */
     public function update(Request $request, $id, $subid)
     {
         $event = Event::where('id', $id)->first();
 
         // check if currently authenticated user is the owner of the book
         if(!PermissionsFacade::eventCanUpdate($event, $request->user())){
-           return response()->json(['error' => 'Cannot add restream to event.'], 403);
+           return response()->json(['error' => 'Permission Denied.'], 403);
         }
 
         $input = $request->all();
 
         if(!$subid) {
-            return response()->json(['error' => 'The ID of the stream you want to remove is required.'], 403);
+            return response()->json(['error' => 'The ID of the recording you want to update is required.'], 403);
         }
 
         RecordingsFacade::updateRecording($subid, $input);
@@ -91,14 +88,4 @@ class RecordingController extends Controller
         return new EventFullResource($event);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Recording  $recording
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Recording $recording)
-    {
-        //
-    }
 }
